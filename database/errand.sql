1134aa6d7031772fc6f5c93133487e80868d88e3
C6H8O7iSour <1427231375@qq.com>
2025-11-06 19:26:57 +0800

    配置文件和数据库

---
+++ database/errand.sql
@@ -0,0 +1,114 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 80036 (8.0.36)
+ Source Host           : localhost:3306
+ Source Schema         : errand
+
+ Target Server Type    : MySQL
+ Target Server Version : 80036 (8.0.36)
+ File Encoding         : 65001
+
+ Date: 06/11/2025 11:50:58
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for evaluation
+-- ----------------------------
+DROP TABLE IF EXISTS `evaluation`;
+CREATE TABLE `evaluation`  (
+  `evaluation_id` int NOT NULL AUTO_INCREMENT COMMENT '评价编号',
+  `order_id` int NOT NULL COMMENT '订单编号',
+  `review` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '评价（0 差评，1 好评）',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`evaluation_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '评价表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of evaluation
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for order
+-- ----------------------------
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order`  (
+  `order_id` int NOT NULL AUTO_INCREMENT COMMENT '订单编号',
+  `client_id` int NOT NULL COMMENT '委托人编号',
+  `helper_id` int NULL DEFAULT NULL COMMENT '跑腿员编号',
+  `content` varchar(75) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '订单需求内容',
+  `help_time` datetime NULL DEFAULT NULL COMMENT '订单指定时间',
+  `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '联系电话',
+  `reward` decimal(4, 2) NULL DEFAULT 0.00 COMMENT '悬赏金额',
+  `status` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '订单状态（0 待帮助，1 已接单，2 已完成）',
+  `publish_time` datetime NULL DEFAULT NULL COMMENT '发布时间',
+  `accept_time` datetime NULL DEFAULT NULL COMMENT '接单时间',
+  `complete_time` datetime NULL DEFAULT NULL COMMENT '完成时间',
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '订单表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of order
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `user_id` int NOT NULL AUTO_INCREMENT COMMENT '用户编号',
+  `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '手机号',
+  `username` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户名',
+  `password` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户密码',
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '头像（路径）',
+  `student_id` int NULL DEFAULT NULL COMMENT '学号',
+  `certificate` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '证件（路径）',
+  `role` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '0' COMMENT '角色（0 委托人，1 跑腿员）',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`user_id`) USING BTREE,
+  UNIQUE INDEX `uniq_phone`(`phone` ASC) USING BTREE COMMENT '手机号不可重复'
+) ENGINE = InnoDB AUTO_INCREMENT = 101 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for wallet
+-- ----------------------------
+DROP TABLE IF EXISTS `wallet`;
+CREATE TABLE `wallet`  (
+  `wallet_id` int NOT NULL AUTO_INCREMENT COMMENT '钱包编号',
+  `user_id` int NOT NULL COMMENT '用户编号',
+  `total_income` decimal(6, 2) NULL DEFAULT 0.00 COMMENT '总收益',
+  `balance` decimal(6, 2) NULL DEFAULT 0.00 COMMENT '可提现余额',
+  PRIMARY KEY (`wallet_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '钱包表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of wallet
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for withdrawal
+-- ----------------------------
+DROP TABLE IF EXISTS `withdrawal`;
+CREATE TABLE `withdrawal`  (
+  `withdrawal_id` int NOT NULL AUTO_INCREMENT COMMENT '提现编号',
+  `user_id` int NOT NULL COMMENT '用户编号',
+  `amount` decimal(6, 2) NULL DEFAULT NULL COMMENT '提现金额',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`withdrawal_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '提现表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of withdrawal
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
