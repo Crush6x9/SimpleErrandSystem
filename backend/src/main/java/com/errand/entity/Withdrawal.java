3213088d5f784cc067820504cce969b2b743f504
C6H8O7iSour <1427231375@qq.com>
2025-11-25 15:17:33 +0800

    改进提现功能 申请时记录提现状态为“处理中”，后续再更改为“已完成”

--- backend/src/main/java/com/errand/entity/Withdrawal.java
+++ backend/src/main/java/com/errand/entity/Withdrawal.java
@@ -24,4 +24,7 @@ public class Withdrawal {
 
     @ApiModelProperty(value = "创建时间")
     private LocalDateTime createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    private LocalDateTime updateTime;
 }
--- backend/src/main/java/com/errand/mapper/WithdrawalMapper.java
+++ backend/src/main/java/com/errand/mapper/WithdrawalMapper.java
@@ -2,6 +2,8 @@ package com.errand.mapper;
 
 import com.errand.entity.Withdrawal;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper
@@ -21,4 +23,12 @@ public interface WithdrawalMapper {
      * @return 提现列表
      */
     List<Withdrawal> selectWithdrawalsByUserId(Long userId);
+
+    /**
+     * 更新提现状态为已完成
+     *
+     * @param expiredTime 过期时间
+     * @return 更新记录数
+     */
+    int updateExpiredWithdrawals(LocalDateTime expiredTime);
 }
